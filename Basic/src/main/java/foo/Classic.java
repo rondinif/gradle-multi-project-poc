5e4f79eff72db0e465adde8b857c9a65da1caf7a
Franco Rondini <rondinif@yahoo.it>
2020-06-10 01:07:04 +0200

    removed fault back classloader in basic

--- Basic/src/main/java/foo/Classic.java
+++ Basic/src/main/java/foo/Classic.java
@@ -9,7 +9,7 @@ public class Classic {
         String advancedClassName = "foo.Bar";
         try {
             System.out.println("Classic lookup for class :"+advancedClassName );
-            Class.forName(advancedClassName );
+            Class.forName(advancedClassName);
             System.out.println("Classic: OK");
         } catch (ClassNotFoundException e1) {
             e1.printStackTrace();
@@ -20,38 +20,6 @@ public class Classic {
                 System.out.println(url.getFile());
             }
             System.out.println("---------------------------------");
-            System.out.println("Class lookup again for class:"+advancedClassName );
-
-            URLClassLoader urlClassLoader = (URLClassLoader) cl;
-            DynamicURLClassLoader dynamicURLClassLoader = new DynamicURLClassLoader(urlClassLoader);
-            
-            try {
-                dynamicURLClassLoader.addURL(new URL(
-                    "file:///Users/ronda/projects/so-test/62213471/Advanced/build/classes/java/main/"));
-                System.out.println("dynamicURLClassLoader current paths:");
-                URL[] dynamicUrls = ((URLClassLoader)dynamicURLClassLoader).getURLs();
-                for(URL url: dynamicUrls){
-                    System.out.println(url.getFile());
-                }
-                System.out.println("---------------------------------");
-                Class.forName(advancedClassName ,true, dynamicURLClassLoader);
-                System.out.println("Classic: OK");
-            } catch (MalformedURLException m) {
-                m.printStackTrace();    
-            } catch (ClassNotFoundException e2) {
-                e2.printStackTrace();
-            }
-        }
-    }
-
-    static class DynamicURLClassLoader extends URLClassLoader {
-        public DynamicURLClassLoader(URLClassLoader classLoader) {
-            super(classLoader.getURLs());
-        }
-
-        @Override
-        public void addURL(URL url) {
-            super.addURL(url);
         }
     }
 }
